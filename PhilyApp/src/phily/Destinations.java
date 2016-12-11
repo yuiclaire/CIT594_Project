a2ecc704dfb478174f214a9c9ac8dceae7e8cb5d
Linyan Dai <ldai@seas.upenn.edu>
2016-12-10 20:33:12 -0500

    Update Destinations.java

--- PhilyApp/src/phily/Destinations.java
+++ PhilyApp/src/phily/Destinations.java
@@ -5,24 +5,25 @@ public abstract class Destinations {
 	public String name;
 	public String address;
 	public double rating;
+	public double x;
+	public double y;
+	public String zip;
+	public String phone;
 	
-	public int getObjectId(){
-		return objectID;
-	}
-	public String getName(){
-		return name;
-	}
-	public String getAddress(){
-		return address;
-	}
-	public double getRating(){
-		return rating;
-	}
-	abstract double getX(); 
-	
+	public abstract int getObjectId();
+
+	public abstract String getName();
+
+	public abstract String getAddress();
+
+	public abstract double getRating();
+
+	abstract double getX();
+
 	abstract double getY();
-	
+
 	abstract String getZip();
-	
+
 	abstract String getPhone();
 }
+
