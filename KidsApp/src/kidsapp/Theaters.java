3bf590f53794ac2ce2c18ace0caf89fc2051b919
Tingyudoris <tingyuge@seas.upenn.edu>
2016-12-12 15:55:03 -0500

    Update Theaters.java

--- KidsApp/src/kidsapp/Theaters.java
+++ KidsApp/src/kidsapp/Theaters.java
@@ -1,5 +1,8 @@
 package kidsapp;
-
+/**
+ * The Theaters class
+ * This class extends Destinations and implements all the abstract methods of it.
+ */
 public class Theaters extends Destinations{
 
 	public Theaters(int objectID, double x, double y, String name, String address, String zip, String phone){
