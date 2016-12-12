bac525e490dea7b02eb39a2f1380ee84f396c711
Tingyudoris <tingyuge@seas.upenn.edu>
2016-12-12 15:53:17 -0500

    Update Libraries.java

--- KidsApp/src/kidsapp/Libraries.java
+++ KidsApp/src/kidsapp/Libraries.java
@@ -1,7 +1,7 @@
 package kidsapp;
 /**
  * The Libraries class
- * This class extends Destination and implements all the abstract methods of Destination.
+ * This class extends Destinations and implements all the abstract methods of it.
  */
 public class Libraries extends Destinations{
 
