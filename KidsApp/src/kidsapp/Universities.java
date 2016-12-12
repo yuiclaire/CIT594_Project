512fd67ffd6f171f24653ebfe5bde90cb6de884e
Tingyudoris <tingyuge@seas.upenn.edu>
2016-12-12 16:51:03 -0500

    Update Universities.java

--- KidsApp/src/kidsapp/Universities.java
+++ KidsApp/src/kidsapp/Universities.java
@@ -4,7 +4,9 @@ package kidsapp;
  * This class extends Destinations and implements all the abstract methods of it.
  */
 public class Universities extends Destinations{
-
+        /**
+         * Constructor for the Universities class
+         */
 	public Universities(int objectID, double x, double y, String name, String address, String zip, String phone){
 		this.objectID = objectID;
 		this.x = x;
@@ -14,60 +16,66 @@ public class Universities extends Destinations{
 		this.zip = zip;
 		this.phone = phone;	
 	}
-	
+	/**
+         * @return the latitude of Universities
+         */
 	@Override
 	public double getX() {
 		
 		return x;
 	}
-
+        /**
+         * @return the longitude of Universities
+         */
 	@Override
 	public double getY() {
 		
 		return y;
 	}
-	
+	/**
+         * @return the zipcode of Universities
+         */
 	@Override
 	public String getZip() {
 		
 		return zip;
 	}
-	
+	/**
+         * @return the phoneNumber of Universities
+         */
 	@Override
 	public String getPhone() {
 		
 		return phone;
 	}
-
+	/**
+         * @return the objectID of Universities
+         */
 	@Override
 	public int getObjectId() {
 		
 		return objectID;
 	}
-
+	/**
+         * @return the name of Universities
+         */
 	@Override
 	public String getName() {
 		
 		return name;
 	}
-
+	/**
+         * @return the address of Universities
+         */
 	@Override
 	public String getAddress() {
 		
 		return address;
 	}
-
-	@Override
-	public double getRating() {
-		
-		return rating;
-	}
-	
-	@Override
 	/**
-	 * 
-	 * @return Return a String containing all information separated by comma
+	 * @return a String containing all information separated by comma
 	 */
+	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder("");
 		sb.append(x);
