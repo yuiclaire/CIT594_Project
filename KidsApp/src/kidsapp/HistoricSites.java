ca0aa8b4b3d082c2bcf045920b9ce5ecf5c0bb0e
Tingyudoris <tingyuge@seas.upenn.edu>
2016-12-12 16:17:29 -0500

    Update HistoricSites.java

--- KidsApp/src/kidsapp/HistoricSites.java
+++ KidsApp/src/kidsapp/HistoricSites.java
@@ -4,7 +4,9 @@ package kidsapp;
  * This class extends Destinations and implements all the abstract methods of it.
  */
 public class HistoricSites extends Destinations{
-
+        /**
+	 * Constructor for class HistoricSites
+	 */
 	public HistoricSites(int objectID, double x, double y, String name, String address, String zip, String phone){
 		this.objectID = objectID;
 		this.x = x;
@@ -14,58 +16,65 @@ public class HistoricSites extends Destinations{
 		this.zip = zip;
 		this.phone = phone;	
 	}
-	
+	/**
+	 * @return the latitude of HistoricSites
+	 */
 	@Override
 	public double getX() {
 		
 		return x;
 	}
-
+	/**
+	 * @return the longitude of HistoricSites
+	 */
 	@Override
 	public double getY() {
 		
 		return y;
 	}
-	
+	/**
+	 * @return the zipcode of HistoricSites
+	 */
 	@Override
 	public String getZip() {
 		
 		return zip;
 	}
-	
+	/**
+	 * @return the phoneNumber of HistoricSites
+	 */
 	@Override
 	public String getPhone() {
 		
 		return phone;
 	}
-
+        /**
+	 * @return the objectID of HistoricSites
+	 */
 	@Override
 	public int getObjectId() {
 		
 		return objectID;
 	}
-
+        /**
+	 * @return the name of HistoricSites
+	 */
 	@Override
 	public String getName() {
 		
 		return name;
 	}
-
+        /**
+	 * @return the longitude of HistoricSites
+	 */
 	@Override
 	public String getAddress() {
 		
 		return address;
 	}
-
-	@Override
-	public double getRating() {
-		
-		return rating;
-	}
-
 	@Override
 	/**
-	 * @return Return a String containing all information separated by comma
+	 * @return a String containing all information separated by comma
 	 */
 	public String toString() {
 		StringBuilder sb = new StringBuilder("");
